780d9c603115483e12b735e26263384f83077091
dermen <dermendarko@gmail.com>
2016-02-10 14:40:21 -0800

    pyramid is beautiful

--- pyramid.py
+++ pyramid.py
@@ -1,102 +1,104 @@
 from numpy import *
-# makes/rotates pyramid with tip at 0,0,0
+from itertools import cycle
+# makes pyramid with tip at 0,0,0
 # and with the base as the x-y plane
 
-def gen_mat(u,v):
+def make_pyramid( u, v, outfilename, length=6, 
+                    a=4.076, atomname='Au'):
     """
-    http://stackoverflow.com/questions/9423621/3d-rotations-of-a-plane
+    Builds a square-base pyramid with top 
+    vertex at 0,0,0
+    
+    'u,v' are unit vectors defining the base of the pyramid
+    'outfilename' name of output txt file
+    'length' is the number of atoms along a base edge
+    'a' is the FCC lattice constant
 
-    Let u be a normal to plane P1
-    Let v be a normal to plane P2
-
-    This function returns the matrix 
-    which rotates and point in P1 to 
-    a corresponding point in P2
+    saves coordinates in .xyz format
     """
+    planar_spacing = a/2.
+#   size of the thing
+    ndim = 2*length -1
+    
     norm_uv = cross( u,v)
     norm_uv /= linalg.norm(norm_uv)
 
-    rot_ang = arccos( dot( [0,0,1.], norm_uv)) 
-    
-    assert( not all( array([0,0,1.]) == norm_uv ) )
 
-    rot_axis = cross( [0,0,1], norm_uv )
-    rot_axis /= linalg.norm( rot_axis)
-    
-    c = cos(rot_ang)
-    s = sin(rot_ang) 
-    c_ = 1-c
-
-    x = rot_axis[0]
-    y = rot_axis[1]
-    z = rot_axis[2]
-    M1 = [ x*x*c_+c, x*y*c_-z*s, x*z*c_+y*s ]
-    M2 = [ y*x*c_+z*s, y*y*c_+c, y*z*c_-x*s ]
-    M3 = [ z*x*c_-y*s, z*y*c_+x*s, z*z*c_+c ]
-    M = array( [M1,M2,M3] ) 
-
-    return M
-
-# args
-u = [ .5, -.5*sqrt(3), 0 ]
-v = [ .5, .5/sqrt(3), sqrt(6)/3. ] 
-rotM = gen_mat(u,v)
-
-# gold unit
-a = 4.076
-spacing = a/2.
-
-# size of the thing
-length = 20 #argparse bla bla
-nrows,ncols = length*3,length*3
-xcor = [a*i/2. for i in xrange(nrows)]
-ycor = [a*i/2. for i in xrange(ncols)]
-
-centerx = xcor[nrows/2]
-centery = ycor[ncols/2]
-
-# make a mask of
-# 1 0 1 0 1 0 ..
-# 0 1 0 1 0 1 ..
-# 1 0 1 0 1 0 
-# . etc
+    layerA = zeros( (ndim, ndim) )
+    layerB = zeros_like(layerA)
+    layerA[::2, ::2] = 1
+    layerB[1::2, 1::2] = 1
+#e.g. if length==3:
 #
-mask = zeros( (nrows, ncols))
-mask[::2,1::2] = 1
-mask[1::2,0::2] = 1
-mask = mask.astype(bool)
+#                  layerA
+#    array([[ 1.,  0.,  1.,  0.,  1.],
+#           [ 0.,  0.,  0.,  0.,  0.],
+#           [ 1.,  0.,  1.,  0.,  1.],
+#           [ 0.,  0.,  0.,  0.,  0.],
+#           [ 1.,  0.,  1.,  0.,  1.]])
+#
+#                 layerB
+#    array([[ 0.,  0.,  0.,  0.,  0.],
+#           [ 0.,  1.,  0.,  1.,  0.],
+#           [ 0.,  0.,  0.,  0.,  0.],
+#           [ 0.,  1.,  0.,  1.,  0.],
+#           [ 0.,  0.,  0.,  0.,  0.]])
 
-# make coordinates of {100} planes when looking down the 
-# {100} axis , i.e. at the  base
-xyzmap = array( [  [xcor[i], ycor[j], 0] 
-        for i in xrange(nrows) for j in xrange(ncols)] )
-xyzmap = xyzmap.reshape( (nrows, ncols, 3))
+    layerA = layerA.astype(bool)
+    layerB = layerB.astype(bool)
 
-# center the base at x,y = 0,0
-xyzmap[:,:,0] -= centerx
-xyzmap[:,:,1] -= centery
+#   make coordinates of {100} planes when looking down the 
+#   {100} axis , i.e. at the  base
+    coor = [ i* (.5*a/sqrt(2.)) for i in xrange(ndim) ]
+    xyzmap = array( [  [coor[i], coor[j], 0] 
+            for i in xrange(ndim) for j in xrange(ndim)] )
+    xyzmap = xyzmap.reshape( (ndim, ndim, 3))
 
-atoms = []
-for i_plane in xrange( length/2 ):
-    ii = 2*i_plane
-    
-    x1,x2 =  (nrows-ii)/2, (nrows+ii)/2 +1 
-    y1,y2 = (ncols-ii)/2, (ncols+ii)/2 +1 
-    if i_plane%2 == 0:
-        m = mask[x1:x2, y1:y2]
+#   center the base at x,y = 0,0
+    center = coor[ndim/2]
+    xyzmap[:,:,0] -= center 
+    xyzmap[:,:,1] -= center 
+
+#   remap xyz such that it is oriented according to u,v plane
+    X = xyzmap[:,:,0]
+    Y = xyzmap[:,:,1]
+    xyzremap = zeros_like( xyzmap)
+    xyzremap[:,:,0] = u[0]*X + v[0]*Y
+    xyzremap[:,:,1] = u[1]*X + v[1]*Y
+    xyzremap[:,:,2] = u[2]*X + v[2]*Y #norm_uv[0]*X + norm_uv[1]*Y
+
+#   check if the peak is in layerA or layerB 
+#   (in above example peak is in layerA)
+    if layerA[ndim/2,ndim/2]:
+#       infinite generator
+        layer_order = cycle( [layerA, layerB] )
     else:
-        m = ~mask[x1:x2, y1:y2]
-    
-    xyz = xyzmap[ x1:x2, y1:y2][m]
-    xyz[:,2] = spacing * i_plane
-    atoms.extend(xyz)
+        layer_order = cycle( [layerB, layerA] )
+        
+    atoms = []
+    for i_plane in xrange(length):
+        # array slice index
+        ii = 2*i_plane
+        x1,x2 =  (ndim-ii)/2, (ndim+ii)/2 +1 
+        
+        # select the layer
+        layer = layer_order.next()
+        
+        # which points are on the lattice
+        on_lattice = layer[ x1:x2, x1:x2]
+        xyz = xyzremap[ x1:x2, x1:x2][on_lattice]
+        
+        # incremembt along normal vector to u,v
+        stack_vec = planar_spacing * i_plane * norm_uv
+        atoms.extend( xyz+stack_vec)
 
-atoms_txt = [ 'Au ' + ' '.join(map(str,a)) 
-            for a in atoms]
-savetxt('atoms.xyz',atoms_txt, fmt='%s')
+#   convert to string output and save
+    atoms_txt = [ '%s '%atomname + ' '.join(map(str,a)) 
+                for a in atoms]
+    savetxt(outfilename, atoms_txt, fmt='%s')
 
-atoms_rot = map( lambda x: dot(rotM,x) , atoms)
-atoms_rot_txt = [ 'Au ' + ' '.join(map(str,a)) 
-            for a in atoms_rot]
-savetxt('atoms_rot.xyz',atoms_rot_txt, fmt='%s')
-
+if __name__ == "__main__":
+    u = [ .5, -.5*sqrt(3), 0 ]
+    v = [ .5, .5/sqrt(3), sqrt(6)/3. ] 
+    fname = 'atoms_1.xyz'
+    make_pyramid(u,v, fname)
